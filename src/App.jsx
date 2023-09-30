0d6236f457d793c91b21530a38024778c05152c3
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2023-09-30 12:49:29 -0600

    bug fix: added correct syntax for vite .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import ImageGrid from "./components/ImageGrid";
 
+//declare apiKey to take the value from GIPHY_API_KEY from the .env
+const apiKey = import.meta.env.VITE_GIPHY_API_KEY
+
 function App() {
   return (
     <div className="">
